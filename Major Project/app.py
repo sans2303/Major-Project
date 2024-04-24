54166c8f8bfaa2e2446bf70b3b31cafa517fcb25
sans2303 <88536245+sans2303@users.noreply.github.com>
2024-04-24 13:12:54 +0530

    Update app.py

--- Major Project/app.py
+++ Major Project/app.py
@@ -79,5 +79,5 @@ def predict():
     #     return render_template('error.html')    # Render error page
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False)
     
