06e1f07c34e74a3f1c910cfe30ea891fc481a002
sans2303 <88536245+sans2303@users.noreply.github.com>
2024-04-28 19:10:20 +0530

    Update app.py

--- Major Project/app.py
+++ Major Project/app.py
@@ -52,5 +52,5 @@ def predict():
     longitude = request.form.get('longitude')
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
     
